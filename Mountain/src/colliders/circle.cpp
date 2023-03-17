1a85f5f37915b4358e83ac7e8963a3bb7d5bd26f
m.grimardhamel <m.grimardhamel@student.isartdigital.com>
2023-03-17 18:18:11 +0100

    Made some local variables constant

--- Mountain/src/colliders/circle.cpp
+++ Mountain/src/colliders/circle.cpp
@@ -36,13 +36,14 @@ bool mountain::Circle::Intersect(const Vector2& p1, const Vector2& p2) const
 {
     // Code from: https://stackoverflow.com/questions/67116296/is-this-code-for-determining-if-a-circle-and-line-segment-intersects-correct
 
-    float x_linear = p2.x - p1.x;
-    float x_constant = p1.x - Position.x;
-    float y_linear = p2.y - p1.y;
-    float y_constant = p1.y - Position.y;
-    float a = x_linear * x_linear + y_linear * y_linear;
-    float half_b = x_linear * x_constant + y_linear * y_constant;
-    float c = x_constant * x_constant + y_constant * y_constant - SQ(Radius);
+    const float x_linear = p2.x - p1.x;
+    const float x_constant = p1.x - Position.x;
+    const float y_linear = p2.y - p1.y;
+    const float y_constant = p1.y - Position.y;
+    const float a = x_linear * x_linear + y_linear * y_linear;
+    const float half_b = x_linear * x_constant + y_linear * y_constant;
+    const float c = x_constant * x_constant + y_constant * y_constant - SQ(Radius);
+    
     return (
         half_b * half_b >= a * c &&
         (-half_b <= a || c + half_b + half_b + a <= 0) &&
