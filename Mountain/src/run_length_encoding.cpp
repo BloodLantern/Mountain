65d2cc5c224e89f6b962c2985048f38286c2a6a5
cc_hunter_boy <celiangdb@gmail.com>
2023-05-06 21:40:02 +0200

    Fixed RLE

--- Mountain/src/run_length_encoding.cpp
+++ Mountain/src/run_length_encoding.cpp
@@ -4,16 +4,18 @@ std::string mountain::RunLengthEncoding::Encode(const std::string& str)
 {
     std::string letters;
 
-    for (unsigned int j = 0; j < str.size(); ++j)
+    for (size_t j = 0; j < str.size(); ++j)
     {
-        unsigned int count = 1;
+        unsigned char count = 1;
         while (str[j] == str[j + 1])
         {
             count++;
             j++;
+            if (count == 0xFF)
+                break;
         }
-        letters += std::to_string(count);
-        letters.push_back(str[j]);
+        letters += count;
+        letters += str[j];
     }
 
     return letters;
@@ -24,8 +26,7 @@ std::string mountain::RunLengthEncoding::Decode(const std::string& data)
     std::string result;
 
     for (size_t i = 0; i < data.size(); i += 2)
-        for (unsigned char repeat = 0; repeat < data[i]; repeat++)
-            result += data[i + 1];
+        result.append(data[i], data[i + 1]);
 
     return result;
 }
--- Mountain_Tests/src/main.cpp
+++ Mountain_Tests/src/main.cpp
@@ -2,10 +2,15 @@
 
 #include "game_example.hpp"
 
+#include <run_length_encoding.hpp>
+
 int main(int, char*[])
 {
-	test::GameExample game("Mountain tests");
+	/*test::GameExample game("Mountain tests");
 	game.Initialize();
 	game.MainLoop();
-	game.Shutdown();
+	game.Shutdown();*/
+
+	std::cout << mountain::RunLengthEncoding::Encode("0000001110121010") << std::endl;
+	std::cout << mountain::RunLengthEncoding::Decode(mountain::RunLengthEncoding::Encode("0000001110121010")) << std::endl;
 }
