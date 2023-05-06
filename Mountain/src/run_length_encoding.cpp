701d776393b0c659b7009d748c77141497978e94
BloodLantern <maxime.gri.berlioz@gmail.com>
2023-05-06 12:52:11 +0200

    Added an RLE compression class

---
+++ Mountain/src/run_length_encoding.cpp
@@ -0,0 +1,31 @@
+#include "run_length_encoding.hpp"
+
+std::string mountain::RunLengthEncoding::Encode(const std::string& str)
+{
+    std::string letters;
+
+    for (unsigned int j = 0; j < str.size(); ++j)
+    {
+        unsigned int count = 1;
+        while (str[j] == str[j + 1])
+        {
+            count++;
+            j++;
+        }
+        letters += count;
+        letters.push_back(str[j]);
+    }
+
+    return letters;
+}
+
+std::string mountain::RunLengthEncoding::Decode(const std::string& data)
+{
+    std::string result;
+
+    for (size_t i = 0; i < data.size(); i += 2)
+        for (unsigned char repeat = 0; repeat < data[i]; repeat++)
+            result += data[i + 1];
+
+    return result;
+}
