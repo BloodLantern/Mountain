490df45ed50f46d42918e7af4078c1ee546c26f4
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-09-25 09:07:46 +0200

    WIP Add Sprite Component
    Former-commit-id: 7e09dd748c322d89330e0e0f9c91e26b04150c03

---
+++ Mountain/include/Mountain/scene/component/sprite.hpp
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include <string>
+
+#include "Mountain/core.hpp"
+#include "Mountain/resource/texture.hpp"
+#include "Mountain/scene/component/component.hpp"
+
+BEGIN_MOUNTAIN
+
+class Sprite : public Component
+{
+public:
+    Sprite() = default;
+    explicit Sprite(std::string spriteName);
+    Sprite(std::string spriteName, float_t frameDuration);
+    
+    [[nodiscard]]
+    const std::string& GetSpriteName() const;
+    void SetSpriteName(const std::string& spriteName);
+    
+    [[nodiscard]]
+    const std::vector<Pointer<Texture>>& GetSprites() const;
+    
+    [[nodiscard]]
+    size_t GetCurrentSprite() const;
+    
+    [[nodiscard]]
+    float_t GetFrameDuration() const;
+    void SetFrameDuration(float_t frameDuration);
+    
+    [[nodiscard]]
+    float_t GetUpdateTimer() const;
+
+private:
+    std::string m_SpriteName;
+
+    std::vector<Pointer<Texture>> m_Sprites;
+    size_t m_CurrentSprite = 0;
+
+    float_t m_FrameDuration = 0.1f;
+    float_t m_UpdateTimer = 0.f;
+
+    void SetupSprites();
+};
+
+END_MOUNTAIN
