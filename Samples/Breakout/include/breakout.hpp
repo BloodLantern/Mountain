f71c20018b57ff7f264103ebe3bb745974455c53
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-10-24 17:28:41 +0200

    WIP Setup samples

---
+++ Samples/Breakout/include/breakout.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <Mountain/game.hpp>
+
+class Breakout : public Mountain::Game
+{
+public:
+    Breakout(const std::string_view& windowTitle, const Vector2i& windowSize, bool_t vsync)
+        : Game(windowTitle, windowSize, vsync)
+    {
+    }
+
+private:
+    void Initialize() override;
+    void LoadResources() override;
+    void Shutdown() override;
+    void Update() override;
+    void Render() override;
+};
