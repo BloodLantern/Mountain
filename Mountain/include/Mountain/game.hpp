2788236e9a7e0c10aaf06eae26ede3f71975b55e
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-11-18 20:10:06 +0100

    Fix compiler settings for Live++

--- Mountain/include/Mountain/game.hpp
+++ Mountain/include/Mountain/game.hpp
@@ -16,14 +16,13 @@ public:
 
     void Play();
 
-private:
     /// @brief To be called by the user when starting execution. Empty by default,
     ///        can be overridden to initialize custom variables or fields.
     virtual void Initialize();
-    virtual void LoadResources();
     /// @brief Called once. Runs until the window is closed and calls all the
     ///        necessary functions of the game.
     void MainLoop();
+    bool_t NextFrame();
     /// @brief To be called by the user when closing the window. Empty by default,
     ///        can be overridden to destroy custom variables or fields.
     virtual void Shutdown();
@@ -32,6 +31,9 @@ private:
     virtual void Update() = 0;
     /// @brief Called once each frame after Game::Update. To be overridden by the user.
     virtual void Render() = 0;
+
+private:
+    virtual void LoadResources();
 };
 
 END_MOUNTAIN
