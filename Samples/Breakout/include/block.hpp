b57ed1c982919572340ec33ea04dc45c788bcf58
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-11-24 15:54:59 +0100

    Add very basic block collisions

--- Samples/Breakout/include/block.hpp
+++ Samples/Breakout/include/block.hpp
@@ -2,12 +2,14 @@
 
 #include <Mountain/scene/entity.hpp>
 
+#include "ball.hpp"
+
 constexpr float_t BlockSize = 50.f;
 
 class Block : public Mountain::Entity
 {
 public:
-    Block();
+    explicit Block(Ball& ball);
     ~Block() override;
 
     DEFAULT_COPY_MOVE_OPERATIONS(Block)
@@ -15,4 +17,9 @@ public:
     void Update() override;
     void Render() override;
     void RenderDebug() override;
+
+private:
+    Ball* m_Ball = nullptr;
+
+    void CheckBallCollisions() const;
 };
--- Samples/Breakout/src/block.cpp
+++ Samples/Breakout/src/block.cpp
@@ -4,7 +4,8 @@
 
 #include "Mountain/rendering/draw.hpp"
 
-Block::Block()
+Block::Block(Ball& ball)
+    : m_Ball(&ball)
 {
     m_Collider = new Mountain::Hitbox{ Vector2::One() * -BlockSize * 0.5f, Vector2::One() * BlockSize };
     m_Collider->basePosition = &position;
@@ -18,6 +19,8 @@ Block::~Block()
 void Block::Update()
 {
     Entity::Update();
+
+    CheckBallCollisions();
 }
 
 void Block::Render()
@@ -32,4 +35,17 @@ void Block::RenderDebug()
     Entity::RenderDebug();
 
     m_Collider->RenderDebug(Mountain::Color::Red());
+}
+
+void Block::CheckBallCollisions() const
+{
+    const Mountain::Collider& ballCollider = *m_Ball->GetCollider();
+    if (!m_Collider->CheckCollision(ballCollider))
+        return;
+
+    const Vector2 diff = ballCollider.AbsoluteCenter() - position;
+    if (diff.x > diff.y)
+        m_Ball->velocity.x *= -1.f;
+    else
+        m_Ball->velocity.y *= -1.f;
 }
