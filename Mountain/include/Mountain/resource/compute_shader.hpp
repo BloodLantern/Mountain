0aa9b98cc6a1d3c4bde500996ba949ac1b262384
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-12-13 14:04:02 +0100

    Add a ShaderBase class
    Former-commit-id: 4dd44c799b415d106574ffce10642877d3ca1160

--- Mountain/include/Mountain/resource/compute_shader.hpp
+++ Mountain/include/Mountain/resource/compute_shader.hpp
@@ -3,12 +3,11 @@
 #include <Maths/math.hpp>
 
 #include "Mountain/core.hpp"
-#include "Mountain/resource/resource.hpp"
-#include "Mountain/utils/color.hpp"
+#include "Mountain/resource/shader_base.hpp"
 
 BEGIN_MOUNTAIN
 
-class ComputeShader : public Resource
+class ComputeShader : public ShaderBase
 {
 public:
     /// @brief Allowed extensions for compute shaders
@@ -41,76 +40,14 @@ public:
 	/// @brief Unloads the compute shader
 	MOUNTAIN_API void ResetSourceData() override;
 
-	/// @brief Sets an int (signed, 32 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, int32_t value) const;
-
-	/// @brief Sets a bool (signed, 32 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, bool_t value) const;
-
-	/// @brief Sets a float (32 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, float_t value) const;
-
-	/// @brief Sets a Vector2 (2 floats, 64 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Vector2& value) const;
-
-	/// @brief Sets a Vector3 (3 floats, 96 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Vector3& value) const;
-
-	/// @brief Sets a Vector4 (4 floats, 128 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Vector4& value) const;
-
-	/// @brief Sets a Color (4 floats, 128 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Color& value) const;
-
-	/// @brief Sets a Matrix2 (4 floats, 128 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Matrix2& value) const;
-
-	/// @brief Sets a Matrix3 (9 floats, 288 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Matrix3& value) const;
-
-	/// @brief Sets a Matrix (16 floats, 512 bits) variable in a compute shader
-	/// @param keyName Variable name
-	/// @param value Value
-	MOUNTAIN_API void SetUniform(std::string_view keyName, const Matrix& value) const;
-
-	/// @brief Gets the internal id of the compute shader
-	/// @return Id
-	[[nodiscard]]
-	MOUNTAIN_API uint32_t GetId() const;
-
 	MOUNTAIN_API void Dispatch(uint32_t groupsX, uint32_t groupsY, uint32_t groupsZ) const;
 
 	MOUNTAIN_API void SynchronizeImageData() const;
 
 private:
-	uint32_t m_Id = 0;
-
 	std::string m_Code;
 
 	void CheckCompilationError(uint32_t id);
-
-	void CheckLinkError();
-
-	[[nodiscard]]
-	int32_t GetUniformLocation(std::string_view keyName) const;
 };
 
 END_MOUNTAIN
