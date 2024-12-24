fae1e9b3b98345fadb744213a3b0364f11218c7a
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-12-24 11:01:49 +0100

    Add shader compile error logs line contents

--- Mountain/include/Mountain/resource/shader_base.hpp
+++ Mountain/include/Mountain/resource/shader_base.hpp
@@ -59,7 +59,8 @@ namespace Mountain
 	protected:
 		uint32_t m_Id = 0;
 
-		void CheckLinkError();
+		void CheckCompileError(uint32_t id, std::string_view type, const std::string& code) const;
+		void CheckLinkError() const;
 
 		[[nodiscard]]
 		int32_t GetUniformLocation(const char_t* keyName) const;
