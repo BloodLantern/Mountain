b44a67d1534d817aeedd600f69ac3bfdb1827620
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-12-25 16:42:15 +0100

    Fix shader include support for binary resources

---
+++ Samples/GameOfLife/src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "gol.hpp"
+
+USE_DEDICATED_GPU
+
+int main(int32_t, char_t**)
+{
+    Gol* game = new Gol("Conway's Game of Life");
+    game->Initialize();
+    game->MainLoop();
+    game->Shutdown();
+    delete game;
+
+    return 0;
+}
