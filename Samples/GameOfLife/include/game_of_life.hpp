00545dd4ffc5da8174f4cfeaa0f7faf7d7f77c41
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-12-26 18:24:30 +0100

    Add working camera
    Former-commit-id: 55ffe04f7db026d3da487da3502fe709daa20415

---
+++ Samples/GameOfLife/include/game_of_life.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "camera.hpp"
+#include "grid.hpp"
+#include "Mountain/game.hpp"
+#include "Mountain/rendering/render_target.hpp"
+
+class GameOfLife : public Mountain::Game
+{
+public:
+    explicit GameOfLife(const char_t* title);
+
+    void Initialize() override;
+    void Update() override;
+    void Render() override;
+    void Shutdown() override;
+
+private:
+    Grid m_Grid;
+    Camera m_Camera;
+    Mountain::RenderTarget m_RenderTarget;
+};
---
+++ Samples/GameOfLife/src/block.cpp
@@ -0,0 +1,78 @@
+﻿#include "block.hpp"
+
+#include <stdexcept>
+
+#include "Maths/vector2.hpp"
+#include "Mountain/rendering/draw.hpp"
+
+#define CHECK_RANGE(x, y)                                                                                   \
+    if ((x) >= BlockSize || (y) >= BlockSize)                                                               \
+        throw std::invalid_argument{ "Cannot get/set cell with x or y greater than BlockSize in Block" }
+
+Block::Block()
+{
+    for (BlockType& line : m_Lines)
+        line = 0;
+}
+
+void Block::Render(const Vector2 offset, const float_t cellSize) const
+{
+    for (uint8_t y = 0; y < BlockSize; y++)
+    {
+        for (uint8_t x = 0; x < BlockSize; x++)
+        {
+            const bool_t cell = GetCell(x, y);
+            if (!cell) // No need to draw the cell if it's dead
+                continue;
+
+            Mountain::Draw::RectangleFilled(
+                offset + Vector2{ static_cast<float_t>(x) * cellSize, static_cast<float_t>(y) * cellSize },
+                Vector2::One() * cellSize
+            );
+        }
+    }
+
+    if (cellSize > 1.f)
+        Mountain::Draw::Rectangle(offset, Vector2::One() * cellSize * BlockSize, Mountain::Color::DarkGray());
+}
+
+bool_t Block::GetCell(const uint8_t x, const uint8_t y) const
+{
+    CHECK_RANGE(x, y);
+
+    const BlockType bit = 1ull << x;
+    return m_Lines[y] & bit;
+}
+
+void Block::SetCell(const uint8_t x, const uint8_t y, const bool_t value)
+{
+    CHECK_RANGE(x, y);
+
+    if (value)
+        m_Empty = false;
+
+    BlockType& line = m_Lines[y];
+    const BlockType bit = 1ull << x;
+    if (value)
+        line |= bit;
+    else
+        line &= ~bit;
+}
+
+void Block::ToggleCell(const uint8_t x, const uint8_t y)
+{
+    CHECK_RANGE(x, y);
+
+    BlockType& line = m_Lines[y];
+    const BlockType bit = 1ull << x;
+
+    if (!(line & bit))
+        m_Empty = false;
+
+    line ^= bit;
+}
+
+bool_t Block::GetEmpty() const
+{
+    return m_Empty;
+}
