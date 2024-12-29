5538e5bf79b8998de2326389b837f3192a3b0c61
BloodLantern <maxime.gri.berlioz@gmail.com>
2024-12-29 12:38:01 +0100

    Add stopwatch class

---
+++ Mountain/include/Mountain/utils/stopwatch.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <chrono>
+
+#include "Mountain/core.hpp"
+
+namespace Mountain
+{
+    class Stopwatch
+    {
+    public:
+        /// @brief Starts a new stopwatch
+        Stopwatch();
+
+        [[nodiscard]]
+        float_t GetElapsedSeconds() const;
+
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+    };
+}
