ccff4a13b28b4da379b1379a449fbf9fb3a43e25
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-01-05 01:13:34 +0100

    Fix arc rendering
    Former-commit-id: e465aa716b3dc64b25bb248caf8cc5b654665355

--- Mountain/shaders/utils.glsl
+++ Mountain/shaders/utils.glsl
@@ -1,7 +1,4 @@
 
-// Pseudo-random generator
-float Random(vec2 seed) { return fract(sin(dot(seed, vec2(12.9898f, 78.233f))) * 43758.5453f); }
-
 const float PositiveInfinity = 1.f / 0.f;
 const float NegativeInfinity = -1.f / 0.f;
 const float NaN = 0.f / 0.f;
@@ -12,3 +9,6 @@ const float PiOver2 = Pi / 2.f;
 const float PiOver4 = Pi / 4.f;
 const float Deg2Rad = Pi / 180.f;
 const float Rad2Deg = 1.f / Deg2Rad;
+
+// Pseudo-random generator
+float Random(in vec2 seed) { return fract(sin(dot(seed, vec2(12.9898f, 78.233f))) * 43758.5453f); }
