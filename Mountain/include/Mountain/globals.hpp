e36ccabdffb78196f6ef7082d9a743e08b624ccf
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-01-19 23:01:04 +0100

    Add new global parameter to debug break on graphics error
    Former-commit-id: e36117b87f71aa34d831d600d1686fe20903ee5a

--- Mountain/include/Mountain/globals.hpp
+++ Mountain/include/Mountain/globals.hpp
@@ -9,6 +9,14 @@
 
 namespace Mountain
 {
+    /// @brief Whether to <b>not</b> use packaged binary resources
+    /// @details If this is @c true, @c BuiltinShadersPath must be set to the Mountain builtin shaders location.
+    /// This means that instead of using the packaged shader files within the Mountain DLL, the files will instead be loaded from the file system.
+    /// This can be useful in case you need to edit the builtin shaders because you can then use hot-reloading.
     PUBLIC_GLOBAL(bool_t, NoBinaryResources, false);
+    /// @brief The path to the Mountain builtin shaders directory
+    /// @see NoBinaryResources
     PUBLIC_GLOBAL(std::string, BuiltinShadersPath, "");  // NOLINT(readability-redundant-string-init)
+    /// @brief Whether to debug break when an OpenGL error is reported
+    PUBLIC_GLOBAL(bool_t, BreakOnGraphicsError, false);
 }
