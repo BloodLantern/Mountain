09e245b0a36293f0396f0899266e4749e94379b8
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-01-22 10:14:48 +0100

    Fix ResourceManager::LoadAll reloading binary resources

--- Mountain/include/Mountain/rendering/gpu_buffer.hpp
+++ Mountain/include/Mountain/rendering/gpu_buffer.hpp
@@ -8,28 +8,28 @@
 namespace Mountain
 {
     /// @brief Low-level interface for OpenGL buffers
-    struct GpuBuffer
+    struct MOUNTAIN_API GpuBuffer
     {
-        MOUNTAIN_API void Create();
+        void Create();
 
-        MOUNTAIN_API void Delete();
+        void Delete();
 
         /// @brief Shorthand for @code Delete(); Create();@endcode
-        MOUNTAIN_API void Recreate();
+        void Recreate();
 
-        MOUNTAIN_API void SetStorage(int64_t size, const void* data, Graphics::BufferStorageFlags flags = Graphics::BufferStorageFlags::None) const;
+        void SetStorage(int64_t size, const void* data, Graphics::BufferStorageFlags flags = Graphics::BufferStorageFlags::None) const;
 
-        MOUNTAIN_API void SetSubData(int64_t offset, int64_t size, const void* data) const;
+        void SetSubData(int64_t offset, int64_t size, const void* data) const;
 
-        MOUNTAIN_API void SetData(int64_t size, const void* data, Graphics::BufferUsage usage) const;
+        void SetData(int64_t size, const void* data, Graphics::BufferUsage usage) const;
 
-        MOUNTAIN_API void SetDebugName(std::string_view name) const;
-
-        [[nodiscard]]
-        MOUNTAIN_API bool_t GetImmutable() const;
+        void SetDebugName(std::string_view name) const;
 
         [[nodiscard]]
-        MOUNTAIN_API uint32_t GetId() const;
+        bool_t GetImmutable() const;
+
+        [[nodiscard]]
+        uint32_t GetId() const;
 
     private:
         uint32_t m_Id = 0;
--- Mountain/include/Mountain/rendering/gpu_vertex_array.hpp
+++ Mountain/include/Mountain/rendering/gpu_vertex_array.hpp
@@ -7,19 +7,19 @@
 namespace Mountain
 {
     /// @brief Low-level interface for OpenGL vertex arrays
-    struct GpuVertexArray
+    struct MOUNTAIN_API GpuVertexArray
     {
-        MOUNTAIN_API void Create();
+        void Create();
 
-        MOUNTAIN_API void Delete();
+        void Delete();
 
         /// @brief Shorthand for @code Delete(); Create();@endcode
-        MOUNTAIN_API void Recreate();
+        void Recreate();
 
-        MOUNTAIN_API void SetDebugName(std::string_view name) const;
+        void SetDebugName(std::string_view name) const;
 
         [[nodiscard]]
-        MOUNTAIN_API uint32_t GetId() const;
+        uint32_t GetId() const;
 
     private:
         uint32_t m_Id = 0;
