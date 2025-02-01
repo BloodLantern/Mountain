9ad3fa7ee62a3dd332c15511f4ac2b686cedfc80
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-02-01 23:09:13 +0100

    Fix custom target FPS
    Former-commit-id: efb3648a78e33d59cc99be05ff9a4588ade30b02

--- Mountain/include/Mountain/input/time.hpp
+++ Mountain/include/Mountain/input/time.hpp
@@ -4,6 +4,8 @@
 #include <optional>
 
 #include "Mountain/core.hpp"
+#include "Mountain/game.hpp"
+#include "Mountain/utils/stopwatch.hpp"
 
 /// @file time.hpp
 /// @brief Defines the Mountain::Time static class.
@@ -15,16 +17,16 @@ namespace Mountain
     {
         STATIC_CLASS(Time)
 
-        friend class Game;
-
     public:
         MOUNTAIN_API static inline float_t timeScale = 1.f;
 
-        /// @brief The maximum delta time value. This is to prevent clipping during a lag spike
+        /// @brief The maximum delta time value. This is to prevent clipping during a lag spike.
         MOUNTAIN_API static inline float_t maxDeltaTime = 0.1f;
 
         /// @brief If this is above 0, the game updates will be frozen for that amount of time
         MOUNTAIN_API static inline float_t freezeTimer = 0.f;
+
+        MOUNTAIN_API static inline std::optional<double_t> targetFps;
 
         /// @brief Get the total elapsed time
         MOUNTAIN_API static float_t GetTotalTime();
@@ -47,12 +49,6 @@ namespace Mountain
         /// @brief Get the total frame count
         MOUNTAIN_API static uint64_t GetTotalFrameCount();
 
-        /// @brief Get the target FPS. If this isn't set, the game is using vertical synchronization.
-        MOUNTAIN_API static std::optional<uint16_t> GetTargetFps();
-
-        /// @brief Set the target FPS. If the given value is empty, will use vertical synchronization instead.
-        MOUNTAIN_API static void SetTargetFps(std::optional<uint16_t> newTargetFps);
-
         /// @brief Get the target delta time.
         ///
         /// This is either @code 1.f / GetTargetFps()@endcode if @code GetTargetFps().has_value()@endcode is @c true or @code 1.f / Screen::GetRefreshRate()@endcode otherwise.
@@ -63,6 +59,8 @@ namespace Mountain
         /// @brief Get how much time the last frame took. If this is equal to the delta time, then the game is lagging.
         MOUNTAIN_API static float_t GetLastFrameDuration();
 
+        // TODO - Add OnInterval function
+
     private:
         MOUNTAIN_API static inline float_t m_TotalTime = 0.f;
         MOUNTAIN_API static inline float_t m_LastTotalTime = 0.f;
@@ -71,15 +69,25 @@ namespace Mountain
         MOUNTAIN_API static inline float_t m_DeltaTime = 0.f;
         MOUNTAIN_API static inline float_t m_DeltaTimeUnscaled = 0.f;
         MOUNTAIN_API static inline uint64_t m_TotalFrameCount = 0;
-        MOUNTAIN_API static inline std::optional<uint16_t> m_TargetFps;
 
         MOUNTAIN_API static inline float_t m_LastFrameDuration = 0.f;
 
-        static void Initialize();
+        MOUNTAIN_API static inline Stopwatch m_Stopwatch;
 
-        /// @brief Updates the time variables using GLFW.
-        ///        This function should be called exactly once each frame.
+        static void Initialize();
+        static void Shutdown();
+
+        /// @brief Updates the time variables
+        /// @remark This is called at the beginning of a frame by the Game class
         static void Update();
+        /// @brief Used to wait between frames
+        /// @remark This is called at the end of a frame by the Game class.
+        /// @remark This function might not wait if the game is not keeping up, that is
+        /// if the delta time between frames is greater than what the target FPS/VSync aim for.
         static void WaitForNextFrame();
+        static double_t SleepFor(double_t milliseconds);
+
+        friend void Game::Initialize();
+        friend bool_t Game::NextFrame();
     };
 }
