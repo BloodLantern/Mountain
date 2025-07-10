e2456eab4d2eb28305b9fbaeb8551236ff90e2bc
BloodLantern <maxime.gri.berlioz@gmail.com>
2025-07-10 22:46:40 +0200

    Add Pointer unit tests

---
+++ UnitTests/Common.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <gtest/gtest.h>
+
+#define UNIT_TESTS
+
+#include <Mountain/Core.hpp>
+
+using namespace Mountain;  // NOLINT(clang-diagnostic-header-hygiene)
+
+#ifdef COMPILER_MSVC
+#pragma warning(disable: 4834) // discarding return value of function with 'nodiscard' attribute
+#pragma warning(disable: 4723) // potential divide by 0
+#endif
